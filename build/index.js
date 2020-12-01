60a88ebff124ee033c669c3a746b9c5ea503e1be
Joko Susilo <susilo.j8@gmail.com>
2020-12-01 14:17:00 +0700

    Add morgan setup body parser json

--- build/index.js
+++ build/index.js
@@ -4,9 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-// route
-app.route("/").get((req, res) => {
-    res.json({ message: "This is Joko Susilo" });
+const body_parser_1 = __importDefault(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+class App {
+    constructor() {
+        this.app = express_1.default();
+        this.plugins();
+        this.routes();
+    }
+    plugins() {
+        this.app.use(body_parser_1.default.json());
+        this.app.use(morgan_1.default("dev"));
+    }
+    routes() {
+        this.app.route("/").get((req, res) => {
+            res.send("Ini ada route menggunakan typescript with express JS");
+        });
+        this.app.route("/users").post((req, res) => {
+            res.send(req.body);
+        });
+    }
+}
+const port = 8000;
+const app = new App().app;
+app.listen(port, () => {
+    console.log("This application run in port " + port);
 });
-app.listen(8000);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
-const app = express();
+class App {
+	public app: Application;
 
-// route
-app.route("/").get((req, res) => {
-	res.json({ message: "This is Joko Susilo" });
+	constructor() {
+		this.app = express();
+		this.plugins();
+		this.routes();
+	}
+
+	protected plugins(): void {
+		this.app.use(bodyParser.json());
+		this.app.use(morgan("dev"));
+	}
+
+	protected routes(): void {
+		this.app.route("/").get((req: Request, res: Response) => {
+			res.send("Ini ada route menggunakan typescript with express JS");
+		});
+
+		this.app.route("/users").post((req: Request, res: Response) => {
+			res.send(req.body);
+		});
+	}
+}
+
+const port: number = 8000;
+const app = new App().app;
+app.listen(port, () => {
+	console.log("This application run in port " + port);
 });
-
-app.listen(8000);
