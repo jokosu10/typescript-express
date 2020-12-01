96d4aee00e6d67595eaa31b1828c9b7995ea7ce4
Joko Susilo <susilo.j8@gmail.com>
2020-12-01 14:26:49 +0700

    Add all some dependency when we create typescript project

--- build/index.js
+++ build/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
+const compression_1 = __importDefault(require("compression"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 class App {
     constructor() {
         this.app = express_1.default();
@@ -15,6 +18,9 @@ class App {
     plugins() {
         this.app.use(body_parser_1.default.json());
         this.app.use(morgan_1.default("dev"));
+        this.app.use(compression_1.default());
+        this.app.use(helmet_1.default());
+        this.app.use(cors_1.default());
     }
     routes() {
         this.app.route("/").get((req, res) => {
