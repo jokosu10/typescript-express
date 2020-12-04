d9bbbb5a1357f499e94fa8f2382a981455bf94a8
Joko Susilo <susilo.j8@gmail.com>
2020-12-04 10:11:38 +0700

    Validate request in endpoint register

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
+import PasswordHash from "../utils/PasswordHash";
 import user from "../db/models/user";
 const db = require('../db/models');
 
 class AuthController {
 	register = async (req: Request, res: Response): Promise<Response> => {
 		let { username, password } = req.body;
+		const HashPassword: string = await PasswordHash.hash(password);
 
 		const createdUser = await db.user.create({
 			username: username,
-			password: password
+			password: HashPassword
 		});
 
 		return res.json({ message: "Registration user success" });
--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,11 +1,12 @@
 import BaseRoutes from "./BaseRoutes";
 
 import AuthController from "../controllers/AuthController";
+import validate from "../middlewares/AuthValidator";
 
 class AuthRoutes extends BaseRoutes {
 
 	public routes(): void {
-		this.router.post("/register", AuthController.register);
+		this.router.post("/register", validate, AuthController.register);
 		this.router.post("/login", AuthController.login);
 	}
 }
