b362ad4f13148a7615ecc809e65cf88297b521fe
Joko Susilo <susilo.j8@gmail.com>
2020-12-06 13:23:33 +0700

    Create and validate in function todo create

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -1,13 +1,22 @@
 import { Response, Request } from "express";
 import IController from "./ControllerInterface";
+const db = require("../db/models");
 
 class TodoController implements IController {
 	index(req: Request, res: Response): Response {
 		return res.json({ message: "Index" });
 	}
 
-	create(req: Request, res: Response): Response {
-		return res.json({ message: "Create" });
+	create = async (req: Request, res: Response): Promise<Response> => {
+		const { id } = req.app.locals.credential;
+		const { description } = req.body;
+
+		const todo = await db.todo.create({
+			user_id: id,
+			description: description
+		})
+
+		return res.json({ message: "Create todo success", data: todo });
 	}
 
 	show(req: Request, res: Response): Response {
